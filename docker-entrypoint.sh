9996bd128bf11188c7adcd6efeed7ae14205b608
Sander Bollen <sander@sndrtj.eu>
2019-12-22 22:34:17 +0100

    Fix docker-entrypoint.sh to allow other commands

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 
-if [ "${SETI_AT_HOME_KEY}" ]
+# if a command was specified, we run that command. E.g. helpful when we do
+# docker run -it <container> /bin/bash
+if [ "${1}" ]
 then
-  echo '<account><master_url>http://setiathome.berkeley.edu/</master_url><authenticator>'"${SETI_AT_HOME_KEY}"'</authenticator></account>' > /var/lib/boinc-client/account_setiathome.berkeley.edu.xml
-  boinc --dir /var/lib/boinc-client
+  exec "$@"
+# otherwise we attempt to run boinc.
 else
-  echo 'Must set the authentication key with -e SETI_AT_HOME_KEY=<token>'
-  exit 1
+  if [ "${SETI_AT_HOME_KEY}" ]
+  then
+    echo '<account><master_url>http://setiathome.berkeley.edu/</master_url><authenticator>'"${SETI_AT_HOME_KEY}"'</authenticator></account>' > /var/lib/boinc-client/account_setiathome.berkeley.edu.xml
+    boinc --dir /var/lib/boinc-client
+  else
+    echo 'Must set the authentication key with -e SETI_AT_HOME_KEY=<token>'
+    exit 1
+  fi
 fi
